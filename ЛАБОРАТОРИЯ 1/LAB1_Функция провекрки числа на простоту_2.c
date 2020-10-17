ac381280f0fa5aa48d4c2333a7022adcd10e42f9
MaksimusMatO <71430300+MaksimusMatO@users.noreply.github.com>
2020-10-17 13:48:03 +0700

    Update and rename LAB 1/LAB1_Функция провекрки числа на простоту_2.c to ЛАБОРАТОРИЯ 1/LAB1_Функция провекрки числа на простоту_2.c

--- ЛАБОРАТОРИЯ 1/LAB1_Функция провекрки числа на простоту_2.c
+++ ЛАБОРАТОРИЯ 1/LAB1_Функция провекрки числа на простоту_2.c
@@ -2,18 +2,18 @@
 
 int is_simple(int number) {
   int progres = 0;                      //переменная для проверки простоты числа
-  if( number>1 ){
-    for( int i=2 ; progres=0 , i<number ; i++ ){
-      int j=0;   
-        if( number%i==0 ){
-          progres = 1 ;
-          printf("\"1\" число %d сложное\n", progres);
-          break;
-        }
-        else{
-          progres = 0;
-        }      
-      j++;
+  if( number > 1 ){
+    for( int i = 2; progres = 0, i < number; i++ ){
+      int j = 0;   
+      if( number % i == 0 ){
+        progres = 1 ;
+        printf("\"1\" число %d сложное\n", progres);
+        break;
+      }
+      else{
+        progres = 0;
+      }      
+      j++ ;
     }
   }
   if(progres == 0){
@@ -24,7 +24,7 @@ int is_simple(int number) {
 }
 int main(){
   int inp_num;
-  scanf ("%d" ,&inp_num );
+  scanf ("%d" , &inp_num );
   is_simple(inp_num);
   return 0;
 }
