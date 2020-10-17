f382015b24bb3edc731d6b0af4a43527dd54f062
MaksimusMatO <71430300+MaksimusMatO@users.noreply.github.com>
2020-10-17 14:45:30 +0700

    Update LAB1_Функция провекрки числа на простоту_2.c

--- LAB 1/LAB1_Функция провекрки числа на простоту_2.c
+++ LAB 1/LAB1_Функция провекрки числа на простоту_2.c
@@ -1,31 +1,28 @@
 #include <stdio.h>
 
 int is_simple(int number) 
-{
-  int progres = 0;                      //переменная для проверки простоты числа
-  if( number > 1 )
+{                   
+  if( number > 2 )
   {
-    for( int i = 2; progres = 0, i < number; i++ )
-    {
-      int j = 0;   
+    for( int i = 2; i < number; i++ )
+    { 
+      int j = 0;  
       if( number % i == 0 )
       {
-        progres = 1 ;
         printf("\"0\" число %d сложное\n", number);
         return 1;
         break;
-      }
-      else{
-        progres = 0;
-      }      
+      }     
       j++ ;
     }
   }
-  if(progres == 0)
+
   {
     printf("\"1\" число %d простое\n", number);
     return 1;
   }
+  
+  return 0;
 }
 
 int main()
