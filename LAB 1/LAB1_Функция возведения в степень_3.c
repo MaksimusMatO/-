643f6ac3d8bfdde957e2e29716ed3419eeaca3fa
MaksimusMatO <71430300+MaksimusMatO@users.noreply.github.com>
2020-10-17 13:48:51 +0700

    Update LAB1_Функция возведения в степень_3.c

--- LAB 1/LAB1_Функция возведения в степень_3.c
+++ LAB 1/LAB1_Функция возведения в степень_3.c
@@ -1,23 +1,21 @@
 #include <stdio.h>
 #include <math.h>
  
-int power(int number, int power)
-{ 
-  float rez=1;
-  for(int i = 0; i<power; i++)
+int power(int number, int power){ 
+  float rez = 1;
+  for( int i = 0; i < power; i++)
   {
-    rez*=number;
+    rez *= number;
   }
-  printf("%f",rez);
+  printf("%f", rez);
   return 0;
 }
-int main()
-{
-  int N=0,m=0;
+int main(){
+  int N = 0, m = 0;
   printf("Введите число: ");
-  scanf("%d",&N);
+  scanf("%d", &N);
   printf("Введите степень: ");
-  scanf("%d",&m);
+  scanf("%d", &m);
   power(N,m);
   return 0;
 }
