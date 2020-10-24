368459bce62d3b74919e302f1018ec1720968d76
MaksimusMatO <71430300+MaksimusMatO@users.noreply.github.com>
2020-10-24 13:09:39 +0700

    Update LAB2_Функция для поиска n-ого числа фибоначи рекурсией_2.c

--- LAB 2/LAB2_Функция для поиска n-ого числа фибоначи рекурсией_2.c
+++ LAB 2/LAB2_Функция для поиска n-ого числа фибоначи рекурсией_2.c
@@ -1,25 +1,36 @@
 #include <stdio.h>
-long get_fibbonachi_via_recursion(int number);
+long long get_fibbonachi_via_recursion(int number);
 
 int main()
 {
   int N;
   printf("what Fibbonacci number to output :");
   scanf("%d", &N);  
-  printf("%ld ", get_fibbonachi_via_recursion(N) );
+  printf("%lld ", get_fibbonachi_via_recursion(N) );
   getchar(); 
   getchar();
 
   return 0;
 }
 
-long get_fibbonachi_via_recursion(int number) 
+long long get_fibbonachi_via_recursion(int number) 
 {
+	if( number == 0 )
+	{
+
+		return 0;
+	}
   if ( number == 1 || number == 2 )
   {
 
   	return 1;
   }
+
+  static long long array[] = {1, 1};
+  if( array[number] == 0 )
+  {
+ 		array[number] = get_fibbonachi_via_recursion(number - 1) + get_fibbonachi_via_recursion(number - 2); 
+  }
 	
-  return get_fibbonachi_via_recursion(number - 1) + get_fibbonachi_via_recursion(number - 2); 
+  return array[number];
 }
