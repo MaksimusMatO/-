b13b95a14e1be378da70e5c4c096a5f0e85e2dfd
MaksimusMatO <71430300+MaksimusMatO@users.noreply.github.com>
2020-10-24 13:19:49 +0700

    Update LAB2_Функция для поиска n-ого числа фибоначи рекурсией_2.c

--- LAB 2/LAB2_Функция для поиска n-ого числа фибоначи рекурсией_2.c
+++ LAB 2/LAB2_Функция для поиска n-ого числа фибоначи рекурсией_2.c
@@ -15,21 +15,21 @@ int main()
 
 long long get_fibbonachi_via_recursion(int number) 
 {
-	if( number == 0 )
-	{
+  if( number == 0 )
+  {
 
-		return 0;
-	}
+    return 0;
+  }
   if ( number == 1 || number == 2 )
   {
 
-  	return 1;
+    return 1;
   }
 
   static long long array[] = {1, 1};
   if( array[number] == 0 )
   {
- 		array[number] = get_fibbonachi_via_recursion(number - 1) + get_fibbonachi_via_recursion(number - 2); 
+    array[number] = get_fibbonachi_via_recursion(number - 1) + get_fibbonachi_via_recursion(number - 2); 
   }
 	
   return array[number];
