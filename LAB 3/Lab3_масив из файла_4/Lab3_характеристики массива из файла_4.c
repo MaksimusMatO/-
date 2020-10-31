0aa84bfa6319cc850810c5cdbcbcfcc27612b3a5
MaksimusMatO <71430300+MaksimusMatO@users.noreply.github.com>
2020-10-31 16:56:22 +0700

    Update Lab3_характеристики массива из файла_4.c

--- LAB 3/Lab3_масив из файла_4/Lab3_характеристики массива из файла_4.c
+++ LAB 3/Lab3_масив из файла_4/Lab3_характеристики массива из файла_4.c
@@ -3,60 +3,68 @@
 
 int main()
 {
-	FILE *file_array;
-	char array_char[100];
-  	int array_int[100];
-	int size;
-  	int i = 0;
-	int j = 0;
-	file_array = fopen( "file_array.txt", "r" );
-	if( file_array != NULL )
-	{
-		size = fread( array_char, 1, 100, file_array );// запись содержимого файла в масив с выводом успешно прочтённых символов 
-    	int fclose(FILE *file_array);// закрытие файла 
-    	fprintf( stdout, "%s", array_char );
-    	while( i <= (size / 2) )
-    	{
-      		array_int[i] = atoi(&array_char[j]);// перевод из string в integer
-      		//sscanf(&array_char[j], "%d", &array_int[i]);// тоже самое что и предыдущая строка
-     		i++; 
-      		j += 2;
-    	}
-	}
-	else
-	{
-		printf("\nfile was not opened");
-        return 1;
-	}
-        int median = array_int[i / 2];
-	int middle;
-	int maximum = array_int[2];
-	int minimum = array_int[2];
-    	for(int n = 0; n < i; n++)
-	{	
-		if(array_int[n] > maximum)
+        FILE *file_array;
+        char array_char[100];
+        int array_int[100];
+        int size;
+        int i = 0;
+        int j = 0;
+        file_array = fopen( "file_array.txt", "r" );
+        if( file_array != NULL )
         {
-            maximum = array_int[n];
+                size = fread( array_char, 1, 100, file_array );// запись содержимого файла в масив с выводом успешно прочтённых символов 
+                int fclose(FILE *file_array);// закрытие файла 
+                fprintf( stdout, "%s", array_char );
+                while( i <= (size / 2) )
+                {
+                        array_int[i] = atoi(&array_char[j]);// перевод из string в integer
+                        //sscanf(&array_char[j], "%d", &array_int[i]);// тоже самое что и предыдущая строка
+                        i++; 
+                        j += 2;
+                }
         }
-	}
-    	for(int n = 0; n < i; n++)
-	{	
-		if(array_int[n]< minimum)
+        else
         {
-            minimum = array_int[n];
+                printf("\nfile was not opened");
+                return 1;
         }
-	}
-    	for(int n = 0; n < i; n++ )
-	{
-		mmiddle += array_int[n];
-	}
-    	middle = middle / i;
-    	printf("\nmedian = %d", median);
- 	printf("\nminimum = %d", minimum); 
-  	printf("\nmiddle = %d", middle);
-  	printf("\nmaximum = %d", maximum);
-	getchar();
-	getchar();
+        int median;
+        int middle;
+        int maximum = array_int[2];
+        int minimum = array_int[2];
+        for(int n = 0; n < i; n++)
+        {       
+                if(array_int[n] > maximum)
+                {
+                        maximum = array_int[n];
+                }
+        }
+        for(int n = 0; n < i; n++)
+        {       
+                if(array_int[n] < minimum)
+                {
+                        minimum = array_int[n];
+                }
+        }
+        for(int n = 0; n < i; n++ )
+        {
+                middle += array_int[n];
+        }
+        middle = middle / i;
+        if( (i % 2 == 0) )
+        {
+                median = array_int[(i / 2) - 1];
+        }
+        else
+        {
+                median = array_int[i / 2];
+        }
+        printf("\nmedian = %d", median);
+        printf("\nminimum = %d", minimum); 
+        printf("\nmiddle = %d", middle);
+        printf("\nmaximum = %d", maximum);
+        getchar();
+        getchar();
 
-	return 0;
+        return 0;
 }
