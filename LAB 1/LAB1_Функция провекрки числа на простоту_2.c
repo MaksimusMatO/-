0ff265643c26f15325f9f1f7c35d0656a2144da5
MaksimusMatO <71430300+MaksimusMatO@users.noreply.github.com>
2020-10-31 16:26:54 +0700

    Update LAB1_Функция провекрки числа на простоту_2.c

--- LAB 1/LAB1_Функция провекрки числа на простоту_2.c
+++ LAB 1/LAB1_Функция провекрки числа на простоту_2.c
@@ -5,8 +5,9 @@ int is_simple(int number);
 int main()
 {
   int inp_num;
+  printf("enter number\n");
   scanf ("%d", &inp_num );
-  printf("%d", is_simple(inp_num) );
+  printf("\n%d", is_simple(inp_num) );
   getchar(); 
   getchar();
 
@@ -15,17 +16,20 @@ int main()
 
 int is_simple(int number) 
 {
-  if (number > 1)
+  if (number > 3)
   {
     for(int i = 2; i < number; i++)
     {
       if (number % i == 0)
       {
         return 0;
-      }
-      
-      return 1;
+      }       
     }
+    return 1;
+  }
+  else if( (number >= 1) || (number <= 3))
+  {
+      return 1;
   }
   else
   {
