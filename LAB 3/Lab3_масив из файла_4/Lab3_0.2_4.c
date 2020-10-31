5f64e43a45b50d2d68ab6ff77c09dbaec67f010d
MaksimusMatO <71430300+MaksimusMatO@users.noreply.github.com>
2020-10-31 11:57:48 +0700

    Update Lab3_0.2_4.c

--- LAB 3/Lab3_масив из файла_4/Lab3_0.2_4.c
+++ LAB 3/Lab3_масив из файла_4/Lab3_0.2_4.c
@@ -7,37 +7,37 @@ int main()
 {
 	FILE *file_array;
 	char array_char[100];
-  int array_int[100];
+  	int array_int[100];
 	int size;
-  int i = 0, j = 0;
+  	int i = 0, j = 0;
 	file_array = fopen( "all/file_array.txt", "r" );
 	if( file_array != NULL )
 	{
 		size = fread( array_char, 1, 100, file_array );// запись содержимого файла в масив с выводом успешно прочтённых символов 
-    int fclose(FILE *file_array);// закрытие файла 
-    printf("\n\nnot sort array - ");
-    fprintf( stdout, "%s", array_char );
-    while( i <= (size / 2) )
-    {
-      array_int[i] = atoi(&array_char[j]);// перевод из string в integer
-      //sscanf(&array_char[j], "%d", &array_int[i]);// тоже самое что и предыдущая строка
-      i++; 
-      j += 2;
-    }
+    		int fclose(FILE *file_array);// закрытие файла 
+    		printf("\n\nnot sort array - ");
+    		fprintf( stdout, "%s", array_char );
+    		while( i <= (size / 2) )
+    		{
+      			array_int[i] = atoi(&array_char[j]);// перевод из string в integer
+      			//sscanf(&array_char[j], "%d", &array_int[i]);// тоже самое что и предыдущая строка
+     			i++; 
+      			j += 2;
+    		}
 		function_sort_shell( array_int, i);
 	}
 	else
 	{
 		printf("pascal");
 	}
-  printf("\n\nsort array - ");
-  for( i = 0; i <= (size / 2); i++)
-  {
-    printf(" %d", array_int[i]);
-  }
-  printf("\nmin = %d", array_int[0]); 
-  printf("\nmid = %d", array_int[i / 2]);
-  printf("\nmax = %d", array_int[i - 1]);
+  	printf("\n\nsort array - ");
+  	for( i = 0; i <= (size / 2); i++)
+  	{
+    		printf(" %d", array_int[i]);
+  	}
+ 	 printf("\nmin = %d", array_int[0]); 
+  	printf("\nmid = %d", array_int[i / 2]);
+  	printf("\nmax = %d", array_int[i - 1]);
 	getchar();
 	getchar();
 
