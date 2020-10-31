3f007387a213b5faa1847374cc5e7057aa6d4238
MaksimusMatO <71430300+MaksimusMatO@users.noreply.github.com>
2020-10-31 15:41:37 +0700

    Update Lab3_характеристики массива_1.c

--- LAB 3/Lab3_характеристики массива_1.c
+++ LAB 3/Lab3_характеристики массива_1.c
@@ -15,7 +15,7 @@ int main()
         array[i] = rand() % 100;
 	}
     int median;
-	int middle;
+	float middle;
 	int maximum = array[0];
 	int minimum = array[size - 1];
 	for(int i = 0; i < size; i++)
@@ -34,35 +34,20 @@ int main()
 	}
 	for(int i = 0; i < size; i++ )
 	{
-		int cost_max = 0, cost_min = 0;
-		for( int j = 0; j != i; j++ )
-	    {
-	    	if( array[j] != array[i] )
-	    	{
-		        if( array[j] > array[i] )
-		        {
-		   			cost_max++;
-		        }
-		    	else
-		    	{
-		    		cost_min++;
-		    	}
-	    	}
-	    }
-		if( cost_max == cost_min )
-		{
-			middle= array[i];	
-		}
+        middle += array[i];
+        
 	}
+    middle = middle / size;
 	for(int i = 0; i < size; i++ )
     {
         printf("%d ", array[i]);
+        
     }
     median = array[size / 2];
     printf("\nmedian %d\n", median);
     printf("maximum %d\n", maximum);
     printf("minimum %d\n", minimum);
-    printf("middle %d\n", middle);
+    printf("middle %.2f\n", middle);
 
 	return 0;
 }
