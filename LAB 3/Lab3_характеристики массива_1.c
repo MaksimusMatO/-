1c5a04eb2b1c1255adf3bf87c8f242e0e8d5a65b
MaksimusMatO <71430300+MaksimusMatO@users.noreply.github.com>
2020-10-31 18:36:26 +0700

    Update Lab3_характеристики массива_1.c

--- LAB 3/Lab3_характеристики массива_1.c
+++ LAB 3/Lab3_характеристики массива_1.c
@@ -5,53 +5,65 @@
 
 int main()
 {   
-        srand( time(NULL) );
+    	srand( time(NULL) );
 	int size;
-	size = rand() % 20;
+	size = rand() % 20 + 1;
 	int array[size];
 	for( int i = 0; i < size; i++ )
 	{
-                array[i] = rand() % 100;
+        	array[i] = rand() % 100;
 	}
-        int median;
-	float middle;
+    	int median;
+	float middle = 0;
 	int maximum = array[0];
 	int minimum = array[size - 1];
 	for( int i = 0; i < size; i++ )
 	{	
 		if(array[i] > maximum)
-                {
-                   	maximum = array[i];
-                }
+		{
+			maximum = array[i];
+		}
 	}
-        for( int i = 0; i < size; i++ )
+    	for( int i = 0; i < size; i++ )
 	{	
 		if( array[i] < minimum )
-                {
-                        minimum = array[i];
-                }
+		{
+			minimum = array[i];
+		}
 	}
 	for( int i = 0; i < size; i++ )
 	{
-                middle += array[i];        
+        	middle += array[i];        
 	}
-        middle = middle / size;
+    	middle = middle / size;
 	for( int i = 0; i < size; i++ )
-        {
-                printf("%d ", array[i]);       
+    	{
+        	printf("%d ", array[i]);       
+    	}
+    	for(int n = 0; n < size; n++)
+    	{
+        	int cost_max = 0, cost_min = 0;
+        	for(int k = 0; k < n; k++)
+        	{
+		    	if( array[n] > array[k] )
+		    	{
+				cost_max++;
+		    	}
+            		else if( array[n] < array[k] )
+            		{
+                		cost_min++;
+            		}
         }
-        if ( (size % 2) == 0 )
+        if( cost_max == cost_min)
         {
-   	        median = array[(size / 2) - 1];
+            	median = array[n];
         }
-        else
-        {
-                median = array[(size / 2)];
-        }
-        printf("\nmedian %d\n", median);
-        printf("maximum %d\n", maximum);
-        printf("minimum %d\n", minimum);
-        printf("middle %.2f\n", middle);
+        
+    	}
+    	printf("\nmedian %d\n", median);
+    	printf("maximum %d\n", maximum);
+    	printf("minimum %d\n", minimum);
+    	printf("middle %.2f\n", middle);
 
 	return 0;
-} 
+}
