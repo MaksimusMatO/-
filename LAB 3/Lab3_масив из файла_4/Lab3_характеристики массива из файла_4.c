48a95cee30e8efde7cef082d978b395ce87b6e59
MaksimusMatO <71430300+MaksimusMatO@users.noreply.github.com>
2020-10-31 18:30:10 +0700

    Update Lab3_характеристики массива из файла_4.c

--- LAB 3/Lab3_масив из файла_4/Lab3_характеристики массива из файла_4.c
+++ LAB 3/Lab3_масив из файла_4/Lab3_характеристики массива из файла_4.c
@@ -3,68 +3,79 @@
 
 int main()
 {
-        FILE *file_array;
-        char array_char[100];
-        int array_int[100];
-        int size;
-        int i = 0;
-        int j = 0;
-        file_array = fopen( "file_array.txt", "r" );
-        if( file_array != NULL )
+	FILE *file_array;
+	char array_char[100];
+  	int array_int[100];
+	int size;
+  	int i = 0;
+	int j = 0;
+	file_array = fopen( "file_array.txt", "r" );
+	if( file_array != NULL )
+	{
+		size = fread( array_char, 1, 100, file_array );// запись содержимого файла в масив с выводом успешно прочтённых символов 
+    	int fclose(FILE *file_array);// закрытие файла 
+    	fprintf( stdout, "%s", array_char );
+    	while( i <= (size / 2) )
+    	{
+      		array_int[i] = atoi(&array_char[j]);// перевод из string в integer
+      		//sscanf(&array_char[j], "%d", &array_int[i]);// тоже самое что и предыдущая строка
+     		i++; 
+      		j += 2;
+    	}
+	}
+	else
+	{
+		printf("\nfile was not opened");
+        return 1;
+	}
+    int median = array_int[2];
+    for(int n = 0; n <= i; n++)
+    {
+        int cost_max = 0, cost_min = 0;
+        for(int k = 0; k < n; k++)
         {
-                size = fread( array_char, 1, 100, file_array );// запись содержимого файла в масив с выводом успешно прочтённых символов 
-                int fclose(FILE *file_array);// закрытие файла 
-                fprintf( stdout, "%s", array_char );
-                while( i <= (size / 2) )
-                {
-                        array_int[i] = atoi(&array_char[j]);// перевод из string в integer
-                        //sscanf(&array_char[j], "%d", &array_int[i]);// тоже самое что и предыдущая строка
-                        i++; 
-                        j += 2;
-                }
+            if( array_int[n] > array_int[k] )
+            {
+                cost_max++;
+            }
+            else if( array_int[n] < array_int[k] )
+            {
+                cost_min++;
+            }
         }
-        else
+        if( cost_max == cost_min)
         {
-                printf("\nfile was not opened");
-                return 1;
+            median = array_int[n];
         }
-        int median;
-        int middle;
-        int maximum = array_int[2];
-        int minimum = array_int[2];
-        for(int n = 0; n < i; n++)
-        {       
-                if(array_int[n] > maximum)
-                {
-                        maximum = array_int[n];
-                }
-        }
-        for(int n = 0; n < i; n++)
-        {       
-                if(array_int[n] < minimum)
-                {
-                        minimum = array_int[n];
-                }
-        }
-        for(int n = 0; n < i; n++ )
+    }
+	float middle = 0;
+	int maximum = array_int[2];
+	int minimum = array_int[2];
+    for(int n = 0; n < i; n++)
+	{	
+		if(array_int[n] > maximum)
         {
-                middle += array_int[n];
+            maximum = array_int[n];
         }
-        middle = middle / i;
-        if( (i % 2 == 0) )
+	}
+    	for(int n = 0; n < i; n++)
+	{	
+		if(array_int[n]< minimum)
         {
-                median = array_int[(i / 2) - 1];
+            minimum = array_int[n];
         }
-        else
-        {
-                median = array_int[i / 2];
-        }
-        printf("\nmedian = %d", median);
-        printf("\nminimum = %d", minimum); 
-        printf("\nmiddle = %d", middle);
-        printf("\nmaximum = %d", maximum);
-        getchar();
-        getchar();
+	}
+    for(int t = 0; t < i; t++ )
+	{
+		middle += array_int[t];
+	}
+    middle = middle / i;
+    printf("\nmedian = %d", median);
+ 	printf("\nminimum = %d", minimum); 
+  	printf("\nmiddle = %.2f", middle);
+  	printf("\nmaximum = %d", maximum);
+	getchar();
+	getchar();
 
-        return 0;
+	return 0;
 }
