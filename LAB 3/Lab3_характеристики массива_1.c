c9a5688b0ba56d0c7c76dcb743aee76ac055f757
MaksimusMatO <71430300+MaksimusMatO@users.noreply.github.com>
2020-10-31 15:44:30 +0700

    Update Lab3_характеристики массива_1.c

--- LAB 3/Lab3_характеристики массива_1.c
+++ LAB 3/Lab3_характеристики массива_1.c
@@ -5,49 +5,48 @@
 
 int main()
 {   
-    srand( time(NULL) );
+    	srand( time(NULL) );
 	int size;
 	printf("Array size : ");
 	scanf("%d", &size);
 	int array[size];
 	for( int i = 0; i < size; i++ )
 	{
-        array[i] = rand() % 100;
+        	array[i] = rand() % 100;
 	}
-    int median;
+    	int median;
 	float middle;
 	int maximum = array[0];
 	int minimum = array[size - 1];
 	for(int i = 0; i < size; i++)
 	{	
 		if(array[i] > maximum)
-        {
-            maximum = array[i];
-        }
+        	{
+           		maximum = array[i];
+        	}
 	}
-    for(int i = 0; i < size; i++)
+    	for(int i = 0; i < size; i++)
 	{	
 		if(array[i]< minimum)
-        {
-            minimum = array[i];
-        }
+        	{
+            		minimum = array[i];
+        	}
 	}
 	for(int i = 0; i < size; i++ )
 	{
-        middle += array[i];
+        	middle += array[i];
         
 	}
-    middle = middle / size;
+    	middle = middle / size;
 	for(int i = 0; i < size; i++ )
-    {
-        printf("%d ", array[i]);
-        
-    }
-    median = array[size / 2];
-    printf("\nmedian %d\n", median);
-    printf("maximum %d\n", maximum);
-    printf("minimum %d\n", minimum);
-    printf("middle %.2f\n", middle);
+    	{
+        	printf("%d ", array[i]);       
+    	}
+   	median = array[size / 2];
+    	printf("\nmedian %d\n", median);
+    	printf("maximum %d\n", maximum);
+    	printf("minimum %d\n", minimum);
+    	printf("middle %.2f\n", middle);
 
 	return 0;
-} 
+}
